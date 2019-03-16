40bb0197d552cedadafa20716e34f94227b8f03a
Washington <washingtonoliveira441@gmail.com>
2019-03-16 19:34:35 -0300

    Criacao da entidade

---
+++ api_wars/src/main/java/br/com/washington/api/wars/api_wars/document/Planeta.java
@@ -0,0 +1,65 @@
+package br.com.washington.api.wars.api_wars.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Planeta {
+
+	@Id
+	private String id;
+	private String nome;
+	private String clima;
+	private String terreno;
+	private int totalAparicoes;
+
+	public Planeta(String id, String nome, String clima, String terreno, int totalAparicoes) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.clima = clima;
+		this.terreno = terreno;
+		this.totalAparicoes = totalAparicoes;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getClima() {
+		return clima;
+	}
+
+	public void setClima(String clima) {
+		this.clima = clima;
+	}
+
+	public String getTerreno() {
+		return terreno;
+	}
+
+	public void setTerreno(String terreno) {
+		this.terreno = terreno;
+	}
+
+	public int getTotalAparicoes() {
+		return totalAparicoes;
+	}
+
+	public void setTotalAparicoes(int totalAparicoes) {
+		this.totalAparicoes = totalAparicoes;
+	}
+
+}
