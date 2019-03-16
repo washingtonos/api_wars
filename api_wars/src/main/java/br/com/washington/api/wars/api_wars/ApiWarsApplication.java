9fa63ec469009a2b9e0653c117f0f292f3271c97
Washington <washingtonoliveira441@gmail.com>
2019-03-16 18:12:05 -0300

    Criacao da estrutura do projeto

---
+++ api_wars/src/main/java/br/com/washington/api/wars/api_wars/ApiWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.washington.api.wars.api_wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWarsApplication.class, args);
+	}
+
+}
